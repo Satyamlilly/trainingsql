bded65f21573b4c225da5d7b512131ad0851d168
Satyam Singh <singh_satyam1@lilly.com>
2022-09-14 14:15:58 +0530

    first commit

--- project.sql
+++ project.sql
@@ -13,13 +13,26 @@ arrive VARCHAR(20),
 booked BIT);
 
 CREATE TABLE cost
-(number INT,
+(number INT FOREIGN KEY REFERENCES flight(number),
 base INT,
 tax INT,
 other INT);
 
 INSERT INTO flight VALUES
-(1, '12-12-2022', 'BLR', 'DEL',0),
-(2, '12-12-2022', 'DEL', 'BLR',0),
-(3, '12-12-2022', 'BLR', 'HYD',0),
-(4, '12-12-2022', 'HYD', 'BLR',0);
+(1, '2022-12-20', 'BLR', 'DEL',0),
+(2, '2022-12-20', 'DEL', 'BLR',0),
+(3, '2022-12-20', 'BLR', 'HYD',0),
+(4, '2022-12-20', 'HYD', 'BLR',0);
+
+INSERT INTO passenger VALUES
+(11, 'a', 10, 'DEL',123),
+(12, 'b', 20, 'BLR',234),
+(13, 'c', 30, 'HYD',345),
+(14, 'd', 40, 'BLR',456);
+
+INSERT INTO cost VALUES
+(1, 100, 10, 1),
+(2, 200, 20, 2),
+(3, 300, 30, 3),
+(4, 400, 40, 4);
+
